e5084d89ab0a4511481b7d9e18ead4570ea4f2f5
Ryo Somatani <ryo_somatani@outlook.com>
2022-06-08 16:59:04 -0700

    comment: modify comment out

--- JAVA-Lab1/src/Exercise4.java
+++ JAVA-Lab1/src/Exercise4.java
@@ -4,6 +4,12 @@
  * Print the Pattern
  * 
  * Write a program to display the following pattern.
+ * 
+ *    J    a   v     v  a
+ *    J   a a   v   v  a a
+ * J  J  aaaaa   V V  aaaaa
+ *  JJ  a     a   V  a     a
+ *  
  */
 public class Exercise4 {
 
