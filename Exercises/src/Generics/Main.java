ee6142e7920af14246f881ed3784e4f49b499204
Ryo Somatani <ryo_somatani@outlook.com>
2022-06-23 14:50:39 -0700

    refactor: use generics method instead of generics class

--- Exercises/src/Generics/Main.java
+++ Exercises/src/Generics/Main.java
@@ -2,21 +2,22 @@ package Generics;
 
 class Main {
 	public static void main(String[] args) {
-		MaximumTest<Integer> num1 = new MaximumTest<>();
-		MaximumTest<Double> num2 = new MaximumTest<>();
+		int num1 = MaximumTest.maximum(3, 8, 5);
+		double num2 = MaximumTest.maximum(6.6, 8.9, 8.7);
 
-		System.out.printf("Max of %d, %d and %d is %d\n\n", 3, 4, 5, num1.maximum(3, 4, 5));
-		System.out.printf("Max of %.1f,%.1f and %.1f is %.1f\n\n", 6.6, 8.9, 8.7, num2.maximum(6.6, 8.9, 8.7));
+		// MaximumTest
+		System.out.printf("Max of %d, %d and %d is %d\n\n", 3, 4, 5, num1);
+		System.out.printf("Max of %.1f,%.1f and %.1f is %.1f\n\n", 6.6, 8.9, 8.7, num2);
+
+		// MaximumTest2
+		System.out.printf("Max of %d, %d and %d is %d\n\n", 3, 4, 5, MaximumTest2.maximum(3, 4, 5));
+		System.out.printf("Max of %.1f,%.1f and %.1f is %.1f\n\n", 6.6, 8.9, 8.7, MaximumTest2.maximum(6.6, 8.9, 8.7));
 	}
 }
 
-class MaximumTest<T extends Number & Comparable<T>> {
-	// determines the largest of three Comparable objects
+class MaximumTest {
 
-	public MaximumTest() {
-	}
-
-	public T maximum(T x, T y, T z) {
+	public static <T extends Number & Comparable<T>> T maximum(T x, T y, T z) {
 		T max = x; // assume x is initially the largest
 
 		if (y.compareTo(max) > 0) {
@@ -24,6 +25,22 @@ class MaximumTest<T extends Number & Comparable<T>> {
 		}
 
 		if (z.compareTo(max) > 0) {
+			max = z; // z is the largest now
+		}
+		return max; // returns the largest object
+	}
+}
+
+class MaximumTest2 {
+
+	public static <T extends Number> T maximum(T x, T y, T z) {
+		T max = x; // assume x is initially the largest
+
+		if (y.doubleValue() > max.doubleValue()) {
+			max = y; // y is the largest so far
+		}
+
+		if (z.doubleValue() > max.doubleValue()) {
 			max = z; // z is the largest now
 		}
 		return max; // returns the largest object
